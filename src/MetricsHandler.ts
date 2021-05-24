4513673d741e75bbf9543919ea9a3fd92a9a8dc4
0xFFBarrelBoy <20694871+0xFFBarrelBoy@users.noreply.github.com>
2021-05-24 20:01:09 +0930

    Added First Cut of metrics handler

---
+++ src/MetricsHandler.ts
@@ -0,0 +1,39 @@
+export class MetricsHandler {
+    public ticker: string;
+    public average: number;
+    public percentDelta: number;
+    public lastPrice: number;
+
+    private scale: number;
+    private prices: Array<number>;
+    private iter: number;
+
+    constructor(coinTicker: string, scale: number) {
+        this.ticker = coinTicker;
+        this.scale = scale;
+        this.prices = Array<number>(scale).fill(0);
+        this.iter = 0;
+
+        this.average = 0;
+        this.lastPrice = 0;
+        this.percentDelta = 0;
+
+    }
+
+    addPrice(currentPrice: number): void {
+        this.percentDelta = currentPrice / this.lastPrice;
+        this.prices[this.iter] = currentPrice;
+        this.average = this.getAverage();
+
+        this.iter += 1;
+        if (this.iter == this.scale) {
+            this.iter = 0;
+        }
+    }
+
+    getAverage(): number {
+        const total = this.prices.reduce((runningTotal, price) => runningTotal + price, 0);
+        return total / this.scale;
+    }
+
+}
