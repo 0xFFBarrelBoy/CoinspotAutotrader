0b3d1f79ddca2d8a4c5f21cf615d7da4835b9c87
0xFFBarrelBoy <20694871+0xFFBarrelBoy@users.noreply.github.com>
2021-05-31 21:48:22 +0930

    Testing logic flow

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { CoinSpotRESTService } from "./CoinSpotRESTService";
+import { CoinHoldings } from './CoinHoldings';
+import { MetricsHandler } from './MetricsHandler';
 
 let restClient = new CoinSpotRESTService('', '');
+let currentHoldings = new Map<String, CoinHoldings>();
+let marketData = new Map<String, MetricsHandler>();
 
 restClient.getMarketPrices().then(marketTable => {
-    console.log(marketTable);
+    for (let key in marketTable) {
+        currentHoldings.set(key, new CoinHoldings());
+    }
+    console.log(marketTable.btc)
+    currentHoldings.get('btc')?.addPurchase(marketTable.btc.bid, 1);
+    console.log(currentHoldings.get('btc')?.getCost());
 });
 
