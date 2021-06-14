cb916afd5469831722c892670fdffed43458ecdc
0xFFBarrelBoy <20694871+0xFFBarrelBoy@users.noreply.github.com>
2021-06-14 21:58:46 +0930

    24 Hour test

--- src/MetricsHandler.ts
+++ src/MetricsHandler.ts
@@ -1,37 +1,61 @@
 export class MetricsHandler {
-    public average: number;
-    public percentDelta: number;
-    public lastPrice: number;
+    private latestPrice: number;
+    private percentDelta: number;
 
     private scale: number;
     private prices: Array<number>;
+
     private iter: number;
+    private leadingIter: number;
 
     constructor(scale: number) {
         this.scale = scale;
         this.prices = Array<number>(scale).fill(0);
         this.iter = 0;
+        this.leadingIter = 1;
 
-        this.average = 0;
-        this.lastPrice = 0;
         this.percentDelta = 0;
-
     }
 
     addPrice(currentPrice: number): void {
-        this.percentDelta = currentPrice / this.lastPrice;
+        this.percentDelta = (1 - currentPrice / this.prices[this.leadingIter]);
         this.prices[this.iter] = currentPrice;
-        this.average = this.getAverage();
-
-        this.iter += 1;
-        if (this.iter == this.scale) {
-            this.iter = 0;
+        this.latestPrice = currentPrice;
+        this.iter = this.leadingIter;
+        this.leadingIter += 1;
+        if (this.leadingIter == this.scale) {
+            this.leadingIter = 0;
         }
     }
 
     getAverage(): number {
-        const total = this.prices.reduce((runningTotal, price) => runningTotal + price, 0);
-        return total / this.scale;
+        let count = 0;
+        let total: number = 0;
+        this.prices.forEach(value => {
+            if (value != 0) {
+                total += +value;
+                count++;
+            }
+        })
+        return MetricsHandler.precisionRound(total / count, 3)
+
     }
 
+    getLatestPrice(): number {
+        return MetricsHandler.precisionRound(this.latestPrice, 3)
+    }
+
+    getDelta(): number {
+        return MetricsHandler.precisionRound(this.percentDelta, 3);
+    }
+
+    private static precisionRound(number: number, precision: number) {
+        if (precision < 0) {
+            let factor = Math.pow(10, precision);
+            return Math.round(number * factor) / factor;
+        }
+        else
+            return +(Math.round(Number(number + "e+" + precision)) +
+                "e-" + precision);
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -8,22 +8,30 @@ let restClient = new CoinSpotRESTService('', '');
 let currentHoldings = new Map<String, CoinHoldings>();
 let marketData = new Map<String, MetricsHandler>();
 
+let tableLog = {};
+restClient.getMarketPrices().then(marketTable => {
+    for (let key in marketTable) {
+        let prices: MarketQuery = marketTable[key];
+        marketData.set(key, new MetricsHandler(24));
+        if (marketData.has(key))
+            marketData.get(key).addPrice(prices.last);
+        tableLog[key] = { 'Last Price': marketData.get(key).getLatestPrice() };
+    };
+    console.table(tableLog);
 
-cron.schedule('* * * * *', () => {
+});
+
+
+cron.schedule('0 * * * *', () => {
     restClient.getMarketPrices().then(marketTable => {
         for (let key in marketTable) {
             let prices: MarketQuery = marketTable[key];
-            if (marketData.has(key) === false) {
-                marketData.set(key, new MetricsHandler(10));
-                console.log('added ' + key + ' ' + prices.last);
-            }
-            else {
-                let metricsHandler = marketData.get(key);
-                if (metricsHandler) {
-                    metricsHandler.addPrice(prices.last);
-                    console.log(key + ' avg: ' + metricsHandler.getAverage());
-                }
+            let metricsHandler = marketData.get(key);
+            if (metricsHandler) {
+                metricsHandler.addPrice(prices.last);
+                tableLog[key] = { 'Last Price': metricsHandler.getLatestPrice(), 'Average': metricsHandler.getAverage(), 'Percent Change': metricsHandler.getDelta() };
             }
         }
+        console.table(tableLog);
     });
 })
